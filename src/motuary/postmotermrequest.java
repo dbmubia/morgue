31458eb732c46bba9fb82a567ee59349932adda0
jnjihia <jnjihia2@gmail.com>
2014-01-24 21:39:24 +0300

    initial commit

---
+++ src/motuary/postmotermrequest.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package motuary;
+
+import javax.swing.JButton;
+import javax.swing.JInternalFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Range_Rover_Sport
+ */
+public class postmotermrequest extends JInternalFrame {
+    
+    private JPanel holder;
+    
+    private JLabel deceasedname;
+    private JLabel admissionno;
+    private JLabel  date_of_arrival;
+    private JLabel  time_o_a;
+    private JLabel  requesters_name;
+    private JLabel  requester_id;
+    private JLabel  requester_tel_no;
+    private JLabel  autopsy_date;
+    private JLabel  autopsy_time;
+    private JLabel nature_of_autopsy;
+    private JLabel name_of_pathologist;
+    
+    
+    private JTextField name_txt;
+    private JTextField admin_txt;
+    private JTextField doa_txt;
+    private JTextField time_txt;
+    private JTextField requester_txt;
+    private JTextField reqid_txt;
+    private JTextField req_tel_txt;
+    private JTextField autopsy_date_txt;
+    private JTextField auto_time;
+    private JTextField pathologist_txt;
+    
+    private JButton btnCancel;
+    private JButton btnbackup;
+    
+    
+    
+    
+    
+    
+    
+    
+}
