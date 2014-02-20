4d1091582948319df68762daef453092d33da0fd
jnjihia <jnjihia2@gmail.com>
2014-02-20 10:13:34 +0300

    initial commit

--- src/motuary/Postmotermcontrol.java
+++ src/motuary/Postmotermcontrol.java
@@ -18,27 +18,23 @@ import javax.swing.plaf.basic.BasicInternalFrameUI;
 
 public class Postmotermcontrol extends JInternalFrame implements ActionListener {
 
-   
     public static JScrollPane CusTableJSP = new JScrollPane();
     public static JPanel JPContainer = new komponenMakeOver.panelmakeOver();
     public JLabel kpano;
-    public static JTable JTCusTable; 
-    
-  
-        
-    
+    public static JTable JTCusTable;
+
     JTextField JHlptxt = new komponenMakeOver.textfieldMakeover();
     JLabel JLHelpText = new JLabel();
 
-    
-    
     JButton JBSearch = new komponenMakeOver.buttonMakeOver();
     JButton JBcheckjobcard = new JButton();
     JButton hd;
     JLabel hdimage;
     JButton JBReload = new JButton("Reload", new ImageIcon("src/images/reload.png"));
     JButton JBExit = new JButton("Exit", new ImageIcon("src/images/EXIT.png"));
-    JButton JBApprove= new JButton("Approve", new ImageIcon("src/images/approve.png"));
+    JButton JBApprove = new JButton("Approve", new ImageIcon("src/images/approve.png"));
+
+    
     
     public static Statement stCus;
     public static Connection conne;
@@ -61,17 +57,19 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
     public Postmotermcontrol() throws SQLException {
 
         initial();
-        setBackground(new Color(61,96,121));
+        setBackground(new Color(61, 96, 121));
         setBounds(screen.width - 1200, 50, 1000, screen.height - 270);
         setLayout(null);
         add(JPContainer);
-         
+
         BasicInternalFrameTitlePane titlePane = (BasicInternalFrameTitlePane) ((BasicInternalFrameUI) this.getUI()).getNorthPane();
         this.remove(titlePane);
     }
 
     private void initial() {
         //-- Add the CusTable
+        Buttonswork listener = new Buttonswork();
+        
         JTCusTable = CreateTable();
         CusTableJSP.getViewport().add(JTCusTable);
         CusTableJSP.setBounds(15, 100, 950, 250);
@@ -81,7 +79,6 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
         kpano.setBounds(220, 60, 170, 25);
         kpano.setFont(new Font("Candara", Font.BOLD, 14));
         kpano.setForeground(new Color(242, 242, 189));
-        
 
         enterdtxt = new komponenMakeOver.textfieldMakeover();
         enterdtxt.setBounds(400, 60, 150, 25);
@@ -93,7 +90,7 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
         JBSearch.setFont(new Font("Dialog", Font.PLAIN, 12));
         JBSearch.setForeground(new Color(242, 242, 189));
         JBSearch.setMnemonic(KeyEvent.VK_S);
-        JBSearch.addActionListener(this);
+        JBSearch.addActionListener(listener);
 
         //-- Add the JBDelete
         JBcheckjobcard.setText("View Job Card");
@@ -101,41 +98,36 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
         JBcheckjobcard.setBounds(100, 380, 140, 35);
         JBcheckjobcard.setFont(new Font("Dialog", Font.PLAIN, 12));
         JBcheckjobcard.setMnemonic(KeyEvent.VK_V);
-        JBcheckjobcard.addActionListener(this);
+        JBcheckjobcard.addActionListener(listener);
 
         //-- Add the JBReload
         JBReload.setBounds(350, 380, 150, 35);
         JBReload.setFont(new Font("Dialog", Font.PLAIN, 12));
         JBReload.setMnemonic(KeyEvent.VK_R);
-        JBReload.addActionListener(this);
-        
-        
+        JBReload.addActionListener(listener);
+
         //Approve button
         JBApprove.setBounds(550, 380, 150, 35);
         JBApprove.setFont(new Font("Dialog", Font.PLAIN, 12));
         JBApprove.setMnemonic(KeyEvent.VK_R);
-        JBApprove.addActionListener(this);
+        JBApprove.addActionListener(listener);
 
         //-- Add the EXITBUTTON
         JBExit.setBounds(750, 380, 150, 35);
         JBExit.setFont(new Font("Dialog", Font.PLAIN, 12));
         JBExit.setMnemonic(KeyEvent.VK_R);
-        JBExit.addActionListener(this);
-       
+        JBExit.addActionListener(listener);
 
         hd = new komponenMakeOver.buttonMakeOverRectanglelogin();
         hd.setText("Click on any record to access the details in the buttons");
         hd.setForeground(new Color(242, 242, 189));
         hd.setBounds(0, 0, 1000, 32);
 
-        
         //
         hdimage = new JLabel(new ImageIcon("src/images/Bimage.png"));
         hdimage.setBounds(2, 2, 32, 32);
-        
+
         //End initialize variables
-        
-        
         //Start set the form properties     
         JPContainer.setLayout(null);
         JPContainer.setBounds(0, 0, 1000, 1000);
@@ -205,7 +197,6 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
 
         NewTable.setPreferredScrollableViewportSize(new Dimension(810, 480));
         NewTable.setBackground(new Color(147, 226, 213));
-        
 
         //Start resize the table column
         NewTable.getColumnModel().getColumn(0).setMaxWidth(200);
@@ -230,12 +221,76 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
         return NewTable;
     }
 
+    //
+//        Object obb = e.getSource();
+//        if (obb == JBcheckjobcard) {
+//            dispose();
+//            if (total != 0) {
+//                try {
+//                    if (JTCusTable.getValueAt(JTCusTable.getSelectedRow(), JTCusTable.getSelectedColumn()) != null) {
+//                        String ObjButtons[] = {"Yes", "No"};
+//                        int PromptResult = JOptionPane.showOptionDialog(null, "Are you sure you want to delete the selected record?", "Delete Record", JOptionPane.DEFAULT_OPTION, JOptionPane.ERROR_MESSAGE, null, ObjButtons, ObjButtons[1]);
+//                        if (PromptResult == 0) {
+//                            String del = "DELETE  FROM members_table WHERE k_p_a_id = " + JTCusTable.getValueAt(JTCusTable.getSelectedRow(), 1);
+//                            System.out.println(del);
+//                            stCus.execute(del);
+//                            reloadRecord();
+//                            JOptionPane.showMessageDialog(null, "Record has been successfully deleted.", "Comfirm Delete", JOptionPane.INFORMATION_MESSAGE);
+//                        }
+//                    }
+//                } catch (Exception sqlE) {
+//                    if (sqlE.getMessage() != null) {
+//                        JOptionPane.showMessageDialog(null, "Please select a record in the list to deleted.", "No Record Selected", JOptionPane.INFORMATION_MESSAGE);
+//                    } else {
+//                        JOptionPane.showMessageDialog(null, "Please select a record in the list to deleted.", "No Record Selected", JOptionPane.INFORMATION_MESSAGE);
+//                    }
+//                }
+//
+//            }
+//        } 
+//        else if (obb == JBReload) {
+//            reloadRecord();
+//        } //Close the Form
+//        else if (obb == JBExit) {
+//            close();
+//            dispose();
+//        }
+//
+//    }
+
+    public static void reloadRecord(String srcSQL) {
+        strSQL = srcSQL;
+        CusTableJSP.getViewport().remove(JTCusTable);
+        JTCusTable = CreateTable();
+        CusTableJSP.getViewport().add(JTCusTable);
+        JPContainer.repaint();
+    }
+
+    public static void reloadRecord() {
+
+        CusTableJSP.getViewport().remove(JTCusTable);
+        JTCusTable = CreateTable();
+        CusTableJSP.getViewport().add(JTCusTable);
+        JPContainer.repaint();
+    }
+
+    public void close() {
+        setVisible(false);
+        dispose();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    public class Buttonswork implements ActionListener{
 
-        Object obb = e.getSource();
-        if (obb == JBcheckjobcard) {
-            if (total != 0) {
+          public void actionPerformed(ActionEvent e) {
+
+            if (e.getSource() == JBSearch) {
+                     if (total != 0) {
                 try {
                     if (JTCusTable.getValueAt(JTCusTable.getSelectedRow(), JTCusTable.getSelectedColumn()) != null) {
                         String ObjButtons[] = {"Yes", "No"};
@@ -257,34 +312,18 @@ public class Postmotermcontrol extends JInternalFrame implements ActionListener 
                 }
 
             }
-        } else if (obb == JBReload) {
-            reloadRecord();
-        } 
-     //Close the Form
-        else if (obb == JBExit) {
-         close();
+            } else if (e.getSource() == JBcheckjobcard) {
+                close();  
+            } else if (e.getSource() == JBReload) {
+               //reloadRecord();
+            }
+            else if (e.getSource() == JBApprove) {
+               //reloadRecord();
+            }
+            else if (e.getSource() == JBExit) {
+               close();
+            }
         }
-
-    }
-
-    public static void reloadRecord(String srcSQL) {
-        strSQL = srcSQL;
-        CusTableJSP.getViewport().remove(JTCusTable);
-        JTCusTable = CreateTable();
-        CusTableJSP.getViewport().add(JTCusTable);
-        JPContainer.repaint();
-    }
-
-    public static void reloadRecord() {
-
-        CusTableJSP.getViewport().remove(JTCusTable);
-        JTCusTable = CreateTable();
-        CusTableJSP.getViewport().add(JTCusTable);
-        JPContainer.repaint();
-    }
-    public void close(){
-        setVisible(false);
-        dispose();
     }
 
 }
